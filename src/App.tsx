83f603cbc45b33e2383fdac2bfd00ab7bbc82783
Santiago Hurtado <sanhurtadolopez@outlook.com>
2023-05-07 06:49:00 -0500

    Mejorado Ui, Agreagda logica de juego, agregado typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,170 +7,206 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
-type Person = {
+interface Person {
   Image: string;
   name: string;
   lastname: string;
   nationality: string;
   age: string;
-};
+}
 
 function App() {
   const [person, setPerson] = useState<Person>({} as Person);
-  const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [age, setAge] = useState("");
-  const [nacionality, setNacionality] = useState("");
-  const logoUrl =
+  const [name, setName] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+  const [nationality, setNationality] = useState<string>("");
+  const logoUrl: string =
     "https://www.kuepa.com/logo-kuepa-01-ce02783ec8555816498a749b2c879fb8.png";
-  const [level, setLevel] = useState(1);
-  const [subLevel, setSubLevel] = useState(1);
+  const [level, setLevel] = useState<number>(1);
+  const [subLevel, setSubLevel] = useState<number>(1);
 
-  const data = [
+  const data: Person[] = [
     {
       Image: "1.jpg",
-      nombre: "Mariela",
-      apellido: "Mirabal",
-      nacionalidad: "Mexicana",
-      edad: "Cinco años",
+      name: "Mariela",
+      lastname: "Mirabal",
+      nationality: "Mexicana",
+      age: "Cinco años",
     },
     {
       Image: "2.jpg",
-      nombre: "Martín",
-      apellido: "Duarte",
-      nacionalidad: "Tailandes",
-      edad: "Diez años",
+      name: "Santiago",
+      lastname: "Duarte",
+      nationality: "Tailandes",
+      age: "Diez años",
     },
     {
       Image: "3.jpg",
-      nombre: "Hatsumi",
-      apellido: "Díaz",
-      nacionalidad: "Colombiana",
-      edad: "Once años",
+      name: "Naomi",
+      lastname: "Díaz",
+      nationality: "Colombiana",
+      age: "Once años",
     },
     {
       Image: "4.jpg",
-      nombre: "Hatsumi",
-      apellido: "Díaz",
-      nacionalidad: "Coreana",
-      edad: "Ocho años",
+      name: "Hatsumi",
+      lastname: "Díaz",
+      nationality: "Coreana",
+      age: "Ocho años",
     },
     {
       Image: "5.jpg",
-      nombre: "Martín",
-      apellido: "Santana",
-      nacionalidad: "Japones",
-      edad: "Siete años",
+      name: "Martín",
+      lastname: "Santana",
+      nationality: "Coreano",
+      age: "Siete años",
     },
     {
       Image: "6.jpg",
-      nombre: "Juan",
-      apellido: "Pérez",
-      nacionalidad: "Mexicana",
-      edad: "Veinticinco años",
+      name: "Luna",
+      lastname: "Pérez",
+      nationality: "Venezolana",
+      age: "Cinco años",
     },
     {
       Image: "7.jpg",
-      nombre: "Luna",
-      apellido: "Gómez",
-      nacionalidad: "Francesa",
-      edad: "Trece años",
+      name: "Simon",
+      lastname: "Gómez",
+      nationality: "Frances",
+      age: "Trece años",
     },
     {
       Image: "8.jpg",
-      nombre: "Simón",
-      apellido: "Vega",
-      nacionalidad: "Australiana",
-      edad: "Nueve años",
+      name: "David",
+      lastname: "Vega",
+      nationality: "Australiano",
+      age: "Nueve años",
     },
     {
       Image: "9.jpg",
-      nombre: "Elena",
-      apellido: "García",
-      nacionalidad: "Alemana",
-      edad: "Dieciocho años",
+      name: "Elena",
+      lastname: "García",
+      nationality: "Alemana",
+      age: "Ocho años",
     },
     {
       Image: "10.jpg",
-      nombre: "Rafael",
-      apellido: "Castro",
-      nacionalidad: "Rusa",
-      edad: "Doce años",
+      name: "Rafael",
+      lastname: "Castro",
+      nationality: "Ruso",
+      age: "Doce años",
     },
     {
       Image: "11.jpg",
-      nombre: "Diana",
-      apellido: "Ramírez",
-      nacionalidad: "Canadiense",
-      edad: "Dieciséis años",
+      name: "Itachi",
+      lastname: "Ramírez",
+      nationality: "Japones",
+      age: "Seis años",
     },
     {
       Image: "12.jpg",
-      nombre: "Oscar",
-      apellido: "López",
-      nacionalidad: "Sudafricana",
-      edad: "Veintidós años",
+      name: "Natalia",
+      lastname: "López",
+      nationality: "Japonesa",
+      age: "Siete años",
     },
     {
       Image: "13.jpg",
-      nombre: "Natalia",
-      apellido: "Fernández",
-      nacionalidad: "Italiana",
-      edad: "Quince años",
+      name: "Belen",
+      lastname: "Fernández",
+      nationality: "Canadiense",
+      age: "Doce años",
     },
     {
       Image: "14.jpg",
-      nombre: "Felipe",
-      apellido: "Hernández",
-      nacionalidad: "Noruega",
-      edad: "Veintiún años",
+      name: "Felipe",
+      lastname: "Hernández",
+      nationality: "Peruano",
+      age: "Nueve años",
     },
     {
-      Image: "15.jpg",
-      nombre: "Verónica",
-      apellido: "Cruz",
-      nacionalidad: "Inglesa",
-      edad: "Diecinueve años",
+      Image: "2.jpg",
+      name: "Camilo",
+      lastname: "Cruz",
+      nationality: "Ingles",
+      age: "Nueve años",
     },
   ];
 
   useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * data.length);
-    setPerson({
-      Image: data[randomIndex].Image,
-      name: data[randomIndex].nombre,
-      lastname: data[randomIndex].apellido,
-      nationality: data[randomIndex].nacionalidad,
-      age: data[randomIndex].edad,
-    });
+
+    resetValues()
+
+    const getRandomPerson = () => {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      return data[randomIndex];
+    };
+
+    const currentPerson = getRandomPerson();
+    setPerson(currentPerson);
 
     if (level < 2) {
-      setLastname(person.lastname);
-      setName(person.name);
+      setName(currentPerson.name);
+      setLastname(currentPerson.lastname);
     } else if (level < 3) {
-      setLastname(person.name);
+      setLastname(currentPerson.lastname);
     }
-  }, []);
+  }, [level, subLevel]);
+
+  function resetValues() {
+    setName("");
+    setLastname("");
+    setAge("");
+    setNationality("");
+  }
 
   function handleOnDropName(e: React.DragEvent) {
-    const name = e.dataTransfer.getData("value");
+    const name: string = e.dataTransfer.getData("value");
     setName(name);
   }
   function handleOnDropLastname(e: React.DragEvent) {
-    const lastname = e.dataTransfer.getData("value");
+    const lastname: string = e.dataTransfer.getData("value");
     setLastname(lastname);
   }
   function handleOnDropAge(e: React.DragEvent) {
-    const age = e.dataTransfer.getData("value");
+    const age: string = e.dataTransfer.getData("value");
     setAge(age);
   }
   function handleOnDropNacionality(e: React.DragEvent) {
-    const age = e.dataTransfer.getData("value");
-    setNacionality(age);
+    const nationality: string = e.dataTransfer.getData("value");
+    setNationality(nationality);
   }
 
   function handleOnDrag(e: React.DragEvent, person: any) {
     e.dataTransfer.setData("value", person.value) as unknown;
+  }
+
+  function handleValidate(params: any) {
+
+    console.log(name);
+    console.log(lastname);
+    console.log(age);
+    console.log(nationality);
+    console.log(person.name);
+    console.log(person.lastname);
+    console.log(person.age);
+    console.log(person.nationality);    
+    if (
+      name == person.name &&
+      lastname == person.lastname &&
+      age == person.age &&
+      nationality == person.nationality
+    ) {
+      if (subLevel < 5) {
+        setSubLevel(subLevel + 1);
+      } else {
+        setSubLevel(0);
+        setLevel(level + 1);
+      }
+    } else {
+      alert("Algo no es correcto");
+    }
   }
 
   return (
@@ -259,7 +295,7 @@ function App() {
                         type="text"
                         onDrop={handleOnDropNacionality}
                         onDragOver={(e) => e.preventDefault()}
-                        value={nacionality}
+                        value={nationality}
                         readOnly
                       />
                     </Col>
@@ -268,8 +304,11 @@ function App() {
               </Row>
             </Container>
           </Col>
-          <Col>
-            <Container>
+          <Col
+            className="d-flex justify-content-center align-items-center"
+            sm={5}
+          >
+            <Container className="text-center">
               {Object.entries(person)
                 .filter(([key]) => key !== "Image")
                 .filter(([key]) =>
@@ -280,20 +319,25 @@ function App() {
                     : true
                 )
                 .map(([key, value]) => (
-                  <li
-                    key={key}
-                    draggable
-                    onDragStart={(e) => handleOnDrag(e, { type: key, value })}
-                  >
-                    {` ${person[key as keyof typeof person]}`}
-                  </li>
+                  <Row>
+                    <Col
+                      key={key}
+                      draggable
+                      onDragStart={(e) => handleOnDrag(e, { type: key, value })}
+                      className="m-2 p-3 bg-blue"
+                    >
+                      {` ${person[key as keyof typeof person]}`}
+                    </Col>
+                  </Row>
                 ))}
             </Container>
           </Col>
         </Row>
-        <Row>
+        <Row className="m-4 text-center">
           <Col>
-            <Button>Validar</Button>
+            <Button className="p-4" onClick={handleValidate}>
+              Validar
+            </Button>
           </Col>
         </Row>
       </Container>
